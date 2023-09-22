b7f55d80c5be9d8574b96b40bfcee8889a3f936f
EduSantiagoR <e.santiago.r05@gmail.com>
2023-09-22 13:56:32 -0600

    Add project files.

---
+++ BL/Disco.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations.Infrastructure;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Disco
+    {
+        public static ML.Result Add(ML.Disco disco)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using(DL.ESantiagoPruebaTecnicaEntities context = new DL.ESantiagoPruebaTecnicaEntities())
+                {
+                    var rowsAffected = context.DiscoAdd(
+                        disco.Titulo,
+                        disco.Artista,
+                        disco.GeneroMusical,
+                        disco.Duracion,
+                        disco.Anio,
+                        disco.Distribuidora);
+                    if(rowsAffected > 0 )
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                        result.ErrorMessage = "No se ha podido agregar.";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+            return result;
+        }
+        public static ML.Result GetAll()
+        {
+            ML.Result result = new ML.Result ();
+            try
+            {
+                using(DL.ESantiagoPruebaTecnicaEntities context = new DL.ESantiagoPruebaTecnicaEntities ())
+                {
+                    var query = context.DiscoGetAll();
+                    if(query != null)
+                    {
+                        result.Objects = new List<object>();
+                        foreach (var registro in query)
+                        {
+                            ML.Disco disco = new ML.Disco();
+                            disco.IdDisco = registro.IdDisco;
+                            disco.Titulo = registro.Titulo;
+                            disco.Artista = registro.Artista;
+                            disco.GeneroMusical = registro.GeneroMusical;
+                            disco.Duracion = registro.GeneroMusical;
+                            disco.Anio = registro.Anio;
+                            disco.Distribuidora = registro.Distribuidora;
+                            disco.Ventas = int.Parse(registro.Ventas.ToString());
+                            disco.Disponibilidad = registro.Disponibilidad;
+                            result.Objects.Add(disco);
+                        }
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                        result.ErrorMessage = "Error al consultar los registros.";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+            return result;
+        }
+    }
+}
---
+++ PL/Controllers/DiscoController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PL.Controllers
+{
+    public class DiscoController : Controller
+    {
+        // GET: Disco
+        public ActionResult GetAll()
+        {
+            ML.Result result = BL.Disco.GetAll();
+            ML.Disco disco = new ML.Disco();
+            disco.Discos = result.Objects;
+            return View(disco);
+        }
+        public ActionResult Form() 
+        { 
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Form(ML.Disco disco)
+        {
+            if(disco.IdDisco == 0) //Add
+            {
+                ML.Result result = BL.Disco.Add(disco);
+                if (result.Correct)
+                {
+                    ViewBag.Mensaje = "Agregado de manera éxitosa.";
+                    return PartialView("Modal");
+                }
+                else
+                {
+                    ViewBag.Mensaje = "Error al agregar.";
+                    return PartialView("Modal");
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
